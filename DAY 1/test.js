0cb1006eb01120414d90ff83f8a40793074c3421
prajjawalmishra05-dev <prajjawalmishra05@gmail.com>
2026-03-02 06:59:07 +0000

    my first change

---
+++ DAY 1/test.js
@@ -0,0 +1,1 @@
+console.log("NIKHIL BETA KISE HO?");
---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1>nukhil</h1>
+      <Employee></Employee>
+
+
+    </>
+  )
+}
+
+function Employee(){
+  return(
+    <>
+    <h2>I am prajjawal from prayagraj</h2>
+    </>
+  )
+}
+
+export default App
