985525cae396497339b9fb8f1d64a920cafc34f7
prajjawalmishra05-dev <prajjawalmishra05@gmail.com>
2026-03-31 07:31:45 +0000

    my fourth change

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,40 +4,32 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import {Demo} from './component/Demo'
 
-function App() {
-  // const [count, setCount] = useState(0)
-  const name="JHON";
-  let x=10;
-  let y=20;
-  function fun()
-  {
-    return "inside funfunction"
-  }
-  function sum(a,b)
-  {
-    return a+b;
-  }
-  function calc(a,b,op)
-  {
-    
-    
-    return a+b;
-    else
-      return a-b;
-     
-  }
-  return (
+function App(){
+  const[count, setCount]= useState(0)
+
+
+  function callpen()
+    {
+        alert("function called")
+    }
+
+  function fruit()
+    {
+        alert("you are click on apple button")
+    }
+  function fruit()
+    {
+        alert("you are click on mango button")
+    }
+  
+  return(
     
     <div>
-      
-      <h1>User variable with jsx</h1>
-      <h3>value of variable{name}</h3>
-      
-      <h2>{x+y}</h2>
-      <h1>use of function with jsx</h1>
-      <h2>fun(prajjawal)</h2>
-      <h3>example of paramitarised function {sum(20,20)}</h3>
-      <h3>value of output {calc(20,10)}</h3>
+      <button onClick={fruit}>APPLE</button>
+      <button onClick={fruit}>MANGO</button>
+
+      <button onClick={callpen}>click</button>
+      <h1>welcome in new project</h1>
       
     </div>
   )
