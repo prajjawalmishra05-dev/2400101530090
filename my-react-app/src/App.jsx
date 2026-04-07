3411ca58d13bd2284388098619520b5f9032731c
prajjawalmishra05-dev <prajjawalmishra05@gmail.com>
2026-04-07 07:37:48 +0000

    my five change

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,36 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import {Demo} from './component/Demo'
+
 
 function App(){
-  const[count, setCount]= useState(0)
+  let data ="Demo"
+  function updata(){
+    data = "Example"
+    alert(data)
+  }
 
-
-  function callpen()
-    {
-        alert("function called")
-    }
-
-  function fruit()
-    {
-        alert("you are click on apple button")
-    }
-  function fruit()
-    {
-        alert("you are click on mango button")
-    }
-  
   return(
     
     <div>
-      <button onClick={fruit}>APPLE</button>
-      <button onClick={fruit}>MANGO</button>
-
-      <button onClick={callpen}>click</button>
-      <h1>welcome in new project</h1>
-      
+      <h1>Example of arrow functon </h1>
+      <h1>{data}</h1>
+      <button onClick={updata}>click to updata</button>       
     </div>
   )
 }
